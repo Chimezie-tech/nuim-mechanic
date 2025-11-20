e18a52a6cb4d2630c7379d435c277f09c334d1dc
Ekesontech <bsnssmy@gmail.com>
2025-11-20 15:55:51 +0100

    working on routing and vite.congig.js files

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss(),
+    tailwindcss()
   ],
-
-  base: '/nuim-mechanic/',
-
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
